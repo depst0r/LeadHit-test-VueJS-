6cf794570db039bd2a71e1d544d70db333d3f2ce
depstor <depstor90@gmail.com>
2020-10-16 22:44:09 +0300

    concurrently

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/authorization">Авторизация</router-link> |
-      <router-link to="/analitics">Аналитика</router-link>
+      <router-link to="/analytics">Аналитика</router-link>
     </div>
     <router-view />
   </div>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import axios from 'axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -7,6 +8,8 @@ import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
+
+Vue.use(axios)
 
 Vue.use(Vuelidate)
 new Vue({
