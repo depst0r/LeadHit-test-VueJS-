78d451d8e6ba541cb4e0159fe23146026e440233
depstor <depstor90@gmail.com>
2020-10-19 14:23:52 +0300

    chart

--- src/views/Analytics.vue
+++ src/views/Analytics.vue
@@ -1,25 +1,45 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-    <section>
-      {{info}}
-    </section>
+  <div class="stats-wrapper">
+   <h1>Аналитика</h1>
+    <chart :chart-data="datacollection" style="width: 700px; margin: auto"></chart>
   </div>
 </template>
 
 <script>
+import Chart from '../components/Chart'
 
 export default {
-  name: 'analytics',
+  components: {
+    Chart
+  },
   data () {
     return {
-      info: null
+      datacollection: {},
+      posts: []
     }
   },
-  mounted () {
-    fetch('http://localhost:3030/visits')
-      .then(res => res.json())
-      .then(res => (this.info = res))
+  async mounted () {
+    const res = await fetch('http://localhost:3030/visits')
+    const posts = await res.json()
+    this.posts = posts
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: this.posts.map(res => res.date),
+        datasets: [
+          {
+            label: 'Аналитика по визитам',
+            borderColor: 'MediumSlateBlue',
+            pointBackgroundColor: 'Aqua',
+            pointBorderColor: 'Aquamarine',
+            backgroundColor: 'SkyBlue',
+            data: this.posts.map(res => res.visits)
+          }
+        ]
+      }
+    }
   }
 }
 </script>
--- src/views/Authorization.vue
+++ src/views/Authorization.vue
@@ -1,39 +1,55 @@
 <template>
   <div>
-  <h1>LeadHit</h1>
-  <form class="card auth-card" @submit.prevent="submitHandler">
-  <div class="card-content">
-    <span class="card-title"></span>
-    <div class="input-field">
-      <input
-          id="email"
-          type="text"
-          v-model.trim="email" :class="{invalid: ($v.email.$dirty && !$v.email.required) || ($v.email.$dirty && !$v.email.minLength) || ($v.email.$dirty && !$v.email.maxLength)}"
-      >
-      <label for="email">Leadhit-Site-Id</label>
-      <small class="helper-text invalid"
-      v-if="$v.email.$dirty && !$v.email.required"
-      >id сайта должен содержать 24 символа</small>
-            <small class="helper-text invalid"
-      v-else-if="$v.email.$dirty && !$v.email.maxLength"
-      >id сайта должен содержать "НЕ БОЛЕЕ"  {{$v.email.$params.maxLength.max}} символов. Сейчас он {{email.length}}</small>
-                  <small class="helper-text invalid"
-      v-else-if="$v.email.$dirty && !$v.email.minLength"
-      >id сайта должен содержать "НЕ МЕНЕЕ" {{$v.email.$params.minLength.min}} символов. Сейчас он {{email.length}}</small>
-    </div>
+    <h1>Авторизация</h1>
+    <form class="card auth-card" @submit.prevent="submitHandler">
+      <div class="card-content">
+        <span class="card-title"></span>
+        <div class="input-field">
+          <input
+            id="email"
+            type="text"
+            v-model.trim="email"
+            :class="{
+              invalid:
+                ($v.email.$dirty && !$v.email.required) ||
+                ($v.email.$dirty && !$v.email.minLength) ||
+                ($v.email.$dirty && !$v.email.maxLength),
+            }"
+          />
+          <label for="email">Leadhit-Site-Id</label>
+          <small
+            class="helper-text invalid"
+            v-if="$v.email.$dirty && !$v.email.required"
+            >id сайта должен содержать 24 символа</small
+          >
+          <small
+            class="helper-text invalid"
+            v-else-if="$v.email.$dirty && !$v.email.maxLength"
+            >id сайта должен содержать "НЕ БОЛЕЕ"
+            {{ $v.email.$params.maxLength.max }} символов. Сейчас он
+            {{ email.length }}</small
+          >
+          <small
+            class="helper-text invalid"
+            v-else-if="$v.email.$dirty && !$v.email.minLength"
+            >id сайта должен содержать "НЕ МЕНЕЕ"
+            {{ $v.email.$params.minLength.min }} символов. Сейчас он
+            {{ email.length }}</small
+          >
+        </div>
+      </div>
+      <div class="card-action">
+        <div>
+          <button
+            class="btn waves-effect waves-light auth-submit"
+            type="submit"
+          >
+            Войти
+          </button>
+        </div>
+      </div>
+    </form>
   </div>
-  <div class="card-action">
-    <div>
-      <button
-          class="btn waves-effect waves-light auth-submit"
-          type="submit"
-      >
-        Войти
-      </button>
-    </div>
-  </div>
-</form>
-</div>
 </template>
 
 <script>
@@ -60,15 +76,11 @@ export default {
           'Api-Key': '5f8475902b0be670555f1bb3:eEZn8u05G3bzRpdL7RiHCvrYAYo',
           'Leadhit-Site-Id': this.email
         }
-      }).then(res => {
+      }).then((res) => {
         if (res.status === 200) {
           localStorage.setItem('Leadhit-Site-Id', this.email)
         }
       })
-      const formData = {
-        email: this.email
-      }
-      console.log(formData)
       if (this.email === '5f8475902b0be670555f1bb3') {
         this.$router.push('/analytics')
       }
